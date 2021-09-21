901e35a8649b05df7a39531b94371dfd10737e93
SigmundGjengedal <sigmund.gjengedal@gmail.com>
2021-09-21 16:32:24 +0200

    test shouldRespondWith200ForKnownRequestTarget() 🔴

--- src/test/java/no/kristiania/HttpServerTest.java
+++ src/test/java/no/kristiania/HttpServerTest.java
@@ -18,10 +18,16 @@ class HttpServerTest {
 
     @Test
     void ShouldRespondWithRequestTargetIn404() throws IOException {
-        //må starte en server som skal få request
         HttpServer server = new HttpServer(1992);
-        //client skal connecte til samme server:
         HttpClient client = new HttpClient("localhost",server.getPort(),"/non-existing");
         assertEquals("File not found: /non-existing",client.getMessageBody());
+    }
+
+    @Test
+    void shouldRespondWith200ForKnownRequestTarget() throws IOException {
+        HttpServer server = new HttpServer(1993);
+        HttpClient client = new HttpClient("localhost",server.getPort(),"/hello");
+        assertEquals(200, client.getStatusCode());
+        assertEquals("Hello World", client.getMessageBody());
     }
 }
