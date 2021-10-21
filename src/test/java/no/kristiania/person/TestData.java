a2ba27e3758eaadfc81f9426084cc9551ee357ba
SigmundGjengedal <sigmund.gjengedal@gmail.com>
2021-10-21 19:29:26 +0200

    added flyway

--- src/test/java/no/kristiania/person/TestData.java
+++ src/test/java/no/kristiania/person/TestData.java
@@ -1,5 +1,6 @@
 package no.kristiania.person;
 
+import org.flywaydb.core.Flyway;
 import org.h2.jdbcx.JdbcDataSource;
 
 import javax.sql.DataSource;
@@ -9,6 +10,7 @@ public class TestData {
     public static DataSource TestDataSource() {
         JdbcDataSource dataSource = new JdbcDataSource();
         dataSource.setURL("jdbc:h2:mem:persondb");
+        Flyway.configure().dataSource(dataSource).load().migrate();
         return dataSource;
     }
 }
