e7a7889fe80bc5e030cfd0f1aaa9fd6119d31e87
SigmundGjengedal <sigmund.gjengedal@gmail.com>
2021-10-21 20:23:48 +0200

    shouldListSavedRoles()

--- src/main/java/no/kristiania/person/RoleDao.java
+++ src/main/java/no/kristiania/person/RoleDao.java
@@ -16,8 +16,13 @@ public class RoleDao {
             this.dataSource = testDataSource;
     }
 
-    public void save(String roleName) {
-
+    public void save(String roleName) throws SQLException {
+        try (Connection connection = dataSource.getConnection()) {
+            try (PreparedStatement statement = connection.prepareStatement("insert into roles (name) values(?)")) {
+                statement.setString(1,roleName);
+                statement.executeUpdate();
+            }
+        }
     }
 
     public List<String> listAll() throws SQLException {
