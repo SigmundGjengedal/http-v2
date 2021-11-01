9f9138beccd5b010ff5950327863f93094d856e3
SigmundGjengedal <sigmund.gjengedal@gmail.com>
2021-11-01 13:33:41 +0100

    Extract httpServer main to PersonnelServer

--- src/main/java/no/kristiania/http/HttpServer.java
+++ src/main/java/no/kristiania/http/HttpServer.java
@@ -19,7 +19,6 @@ public class HttpServer {
 
     private final ServerSocket serverSocket;
     private final HashMap<String, HttpController> controllers = new HashMap<>();
-    private static final Logger logger = LoggerFactory.getLogger(HttpServer.class);
 
 
     public HttpServer(int serverPort) throws IOException {
@@ -64,6 +63,7 @@ public class HttpServer {
             fileTarget = requestTarget;
         }
 
+        // her avgjøres det om vi har et respons:
         if (controllers.containsKey(fileTarget)) {
             HttpMessage response = controllers.get(fileTarget).handle(httpMessage);
             response.write(clientSocket);
@@ -133,39 +133,6 @@ public class HttpServer {
 
 
  // ****************** MAIN
-
-    public static void main(String[] args) throws IOException {
-        DataSource dataSource = createDataSource();
-        RoleDao roleDao = new RoleDao(dataSource);
-        PersonDao personDao = new PersonDao(dataSource);
-
-        HttpServer httpServer = new HttpServer(1800);
-        // hvor vi adder data til databasen. En controller gis en tilsvarende dao:
-        httpServer.addController("/api/roleOptions", new RoleOptionsController(roleDao));
-        httpServer.addController("/api/newPerson", new AddPersonController(personDao));
-        logger.info("Starting http://localhost:{}/index.html",httpServer.getPort());
-
-    }
-
-    private static DataSource createDataSource() throws IOException {
-        Properties properties = new Properties();
-        try (FileReader fileReader = new FileReader("pgr203.properties")) {
-            properties.load(fileReader);
-        }
-        PGSimpleDataSource dataSource = new PGSimpleDataSource();
-        dataSource.setURL(properties.getProperty("dataSource.url",
-                "jdbc:postgresql://localhost:5432/person_db2"
-        ));
-
-        dataSource.setUser(properties.getProperty("dataSource.user",
-                "person_dbuser2"
-        ));
-
-        dataSource.setPassword(properties.getProperty("dataSource.password"));
-
-        Flyway.configure().dataSource(dataSource).load().migrate();
-        return dataSource;
-    }
 
     public void addController(String path, HttpController controller) {
         controllers.put(path, controller);
---
+++ src/main/java/no/kristiania/http/PersonnelServer.java
@@ -0,0 +1,51 @@
+package no.kristiania.http;
+
+import no.kristiania.person.PersonDao;
+import no.kristiania.person.RoleDao;
+import org.flywaydb.core.Flyway;
+import org.postgresql.ds.PGSimpleDataSource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.sql.DataSource;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Properties;
+
+public class PersonnelServer {
+
+    private static final Logger logger = LoggerFactory.getLogger(HttpServer.class);
+
+    public static void main(String[] args) throws IOException {
+        DataSource dataSource = createDataSource();
+        RoleDao roleDao = new RoleDao(dataSource);
+        PersonDao personDao = new PersonDao(dataSource);
+
+        HttpServer httpServer = new HttpServer(1800);
+        // hvor vi adder data til databasen. En controller gis en tilsvarende dao:
+        httpServer.addController("/api/roleOptions", new RoleOptionsController(roleDao));
+        httpServer.addController("/api/newPerson", new AddPersonController(personDao));
+        logger.info("Starting http://localhost:{}/index.html",httpServer.getPort());
+
+    }
+
+    private static DataSource createDataSource() throws IOException {
+        Properties properties = new Properties();
+        try (FileReader fileReader = new FileReader("pgr203.properties")) {
+            properties.load(fileReader);
+        }
+        PGSimpleDataSource dataSource = new PGSimpleDataSource();
+        dataSource.setURL(properties.getProperty("dataSource.url",
+                "jdbc:postgresql://localhost:5432/person_db2"
+        ));
+
+        dataSource.setUser(properties.getProperty("dataSource.user",
+                "person_dbuser2"
+        ));
+
+        dataSource.setPassword(properties.getProperty("dataSource.password"));
+
+        Flyway.configure().dataSource(dataSource).load().migrate();
+        return dataSource;
+    }
+}
